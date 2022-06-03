f0e25a68c9b7b1a86e8123400aeee65b62a994d8
aakashdeveloper <ahanda205@gmail.com>
2022-06-03 09:49:35 +0530

    binding

--- firstapp/src/app/home/Search.component.ts
+++ firstapp/src/app/home/Search.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {ICity} from './location.model'
 
 @Component({
     selector: 'app-search',
@@ -6,4 +7,46 @@ import {Component} from '@angular/core';
     styleUrls: ['./Search.component.css']
 })
 
-export class SearchComponent {}
+export class SearchComponent {
+    title: String = 'Find Best Place Near You'
+    keyWord: String = 'Test Date'
+    textNumber : Number = 1
+    locationData: ICity[] = [
+        {
+          "_id": "6187738a62a1816f8858634e",
+          "location_id": 4,
+          "location_name": "Bibvewadi, Pune",
+          "state_id": 2,
+          "state": "Maharashtra",
+          "country_name": "India"
+        },
+        {
+          "_id": "6187738a62a1816f8858634d",
+          "location_id": 1,
+          "location_name": "Ashok Vihar Phase 3, New Delhi",
+          "state_id": 1,
+          "state": "Delhi",
+          "country_name": "India"
+        },
+        {
+          "_id": "6187738a62a1816f8858634f",
+          "location_id": 8,
+          "location_name": "Jeevan Bhima Nagar, Bangalore",
+          "state_id": 3,
+          "state": "Karnataka",
+          "country_name": "India"
+        },
+        {
+          "_id": "6187738a62a1816f88586350",
+          "location_id": 13,
+          "location_name": "Sector 13, Chandigarh",
+          "state_id": 4,
+          "state": "Punjab",
+          "country_name": "India"
+        }
+    ]
+
+    handleCity(event:Event){
+      console.log((event.target as HTMLInputElement).value)
+    }
+}
---
+++ firstapp/src/app/home/location.model.ts
@@ -0,0 +1,8 @@
+export interface ICity{
+    _id: string,
+    location_id: number,
+    location_name: string,
+    state_id: number,
+    state: string,
+    country_name: string
+}
