1b29f60c5f4f67ff0937c1bc1936c9ee82928d67
aakashdeveloper <ahanda205@gmail.com>
2022-06-24 09:55:27 +0530

    reactive

--- reactivecode/src/app/employeeForm/employee.component.ts
+++ reactivecode/src/app/employeeForm/employee.component.ts
@@ -2,6 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormBuilder,
         Validators, AbstractControl} from '@angular/forms'
 
+function ratingRange(min: Number, max: Number){
+    return(out: AbstractControl): {[key: string]: boolean} | null => {
+        if(out.value !== undefined && (isNaN(out.value) || out.value < min || out.value > max)){
+            return {range: true}
+        }else{
+            return null;
+        }
+    }
+}
+
 @Component({
     templateUrl: './employee.component.html',
     styleUrls: ['./employee.component.css']
@@ -16,8 +26,26 @@ export class EmployeeFormComponent implements OnInit {
         this.employeeFrom = this.fb.group({
             firstName:['Kevin',[Validators.required,Validators.minLength(3)]],
             lastName:['',[Validators.maxLength(5)]],
-            email:['a@a.com',[Validators.required,Validators.pattern('^([a-zA-Z0-9]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,5})$')]],
+            email:['a@a.com',[Validators.required,
+                              Validators.pattern('^([a-zA-Z0-9]+)@([a-zA-Z0-9]+)\.([a-zA-Z]{2,5})$')]],
+            phone:[''],
+            Notification:[],
+            rating:['3',[Validators.required, ratingRange(1,5)]]
         })
+    }
+
+    setNotification(notifyVia: string):void{
+        const phoneControl = this.employeeFrom.get('phone');
+        if(notifyVia === 'phone'){
+            phoneControl.setValidators(Validators.required)
+        }else{
+            phoneControl.clearValidators()
+        }
+        phoneControl.updateValueAndValidity()
+    }
+
+    submitForm():void{
+        console.log(this.employeeFrom.value)
     }
 }
 
