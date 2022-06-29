fc96cb67ea52f2a2d4d05bc6a1c608dbdaa85e80
aakashdeveloper <ahanda205@gmail.com>
2022-06-29 09:51:29 +0530

    gaurds

--- loginapp/src/app/app-routing.module.ts
+++ loginapp/src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { LoginformComponent } from './loginform/loginform.component'
 import { RegisterformComponent } from './registerform/registerform.component';
 import { ProfileComponent } from './profile/profile.component';
 import { UserListComponent } from './user-list/user-list.component';
+import { ProfileGaurdService } from './gaurds/profilegaurd.service';
+import { UserGaurdServics } from './gaurds/usergaurd.service';
 
 
 const routes: Routes = [
-  {path:'user',component: UserListComponent},
-  {path:'profile',component: ProfileComponent},
+  {path:'user',canActivate:[UserGaurdServics], component: UserListComponent},
+  {path:'profile',canActivate:[ProfileGaurdService],  component: ProfileComponent},
   {path:'login',component: LoginformComponent},
   {path:'register',component: RegisterformComponent},
   {path:'',component: RegisterformComponent}
@@ -16,6 +18,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  providers:[ProfileGaurdService,UserGaurdServics],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ loginapp/src/app/gaurds/usergaurd.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from '@angular/core'
+import { ActivatedRouteSnapshot, CanActivate, Router } from '@angular/router';
+
+@Injectable()
+
+export class UserGaurdServics implements CanActivate{
+    token: string|null = '';
+    role: string|null = '';
+
+    constructor(private router: Router){}
+
+    canActivate(router:ActivatedRouteSnapshot):boolean {
+        this.token = localStorage.getItem('Token_Number');
+        this.role = localStorage.getItem('Role_Type');
+        if(this.token == null ){
+            this.router.navigate(['/login'])
+            return false
+        }
+        if(this.token !== null && this.role === 'Admin' ){
+            return true
+        }
+        if(this.token !== null && this.role === 'User' ){
+            this.router.navigate(['/profile'])
+            return false
+        }
+        return false
+    }
+}
